c9eecf95b0182c2a9732715ede6c52bc97d82e13
Wolfpack <piafyoyo06@gmail.com>
2023-05-04 02:33:47 +0800

    feat: 修正 favicon 的路徑

--- pages/_app.js
+++ pages/_app.js
@@ -18,7 +18,7 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/images/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </>
