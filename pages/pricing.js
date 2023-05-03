3aaa4c170e6bc6ee7c5c6f328685998e72028b62
Wolfpack <piafyoyo06@gmail.com>
2023-05-04 02:29:31 +0800

    feat: 在定價頁面內新增內容

---
+++ pages/pricing.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Pricing() {
+  return (
+    <>
+      <Head>
+        <title>AI 工具王 - 定價</title>
+        <meta
+          name="description"
+          content="2023 軟體工程師體驗營 AI 工具王定價頁"
+        />
+      </Head>
+      <main></main>
+    </>
+  );
+}
