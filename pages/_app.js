52e808a4ee5aae87a1a3fa46dc6d854967c6836e
Wolfpack <piafyoyo06@gmail.com>
2023-05-04 02:02:55 +0800

    feat: 引入 Bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
-import '@/styles/globals.css'
+// 引入 Bootstrap 的樣式檔案
+import "bootstrap/dist/css/bootstrap.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// 引入全域樣式
+import "@/styles/globals.css";
+
+import { useEffect } from "react";
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }) {
+  // 引入 Bootstrap 的程式碼檔案
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap.js");
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/images/favicon.webp" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
