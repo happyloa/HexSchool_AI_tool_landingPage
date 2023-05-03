b218b95029e135e96141a87173e6e4cd6b7f0be8
Wolfpack <piafyoyo06@gmail.com>
2023-05-04 02:02:32 +0800

    refactor: 將部分標籤移動位置

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,6 @@ export default function Home() {
           name="description"
           content="2023 軟體工程師體驗營 AI 工具王登陸頁"
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main></main>
     </>
