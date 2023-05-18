989ed1676379954c1c26d04e3602aa39ff382da1
Wolfpack <piafyoyo06@gmail.com>
2023-05-19 03:21:46 +0800

    feat: 新增網站導覽列

---
+++ components/NavBar.js
@@ -0,0 +1,41 @@
+// 引入 Styled Components 套件
+import styled from "styled-components";
+
+import Image from "next/image";
+import Link from "next/link";
+
+const Nav = styled.nav`
+  margin-bottom: 40px;
+  @media (max-width: 768px) {
+    margin: 0 10px;
+  }
+`;
+
+const DivContainer = styled.div`
+  border-bottom: 1px solid #fff;
+  padding: 10px 0;
+`;
+
+export default function NavBar() {
+  return (
+    <Nav className="navbar sticky-top">
+      <DivContainer className="container">
+        <Link className="navbar-brand" href="/">
+          <Image src="/logo.png" alt="AI 工具王" width={225} height={40} />
+        </Link>
+        <ul className="navbar-nav flex-row">
+          <li className="nav-item me-3">
+            <Link className="nav-link text-white" href="/">
+              首頁
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-white" href="/pricing">
+              定價
+            </Link>
+          </li>
+        </ul>
+      </DivContainer>
+    </Nav>
+  );
+}
