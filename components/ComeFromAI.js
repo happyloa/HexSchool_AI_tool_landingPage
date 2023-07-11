4b234ab6c8feb2ce75b1e412df949b95e4d34aa3
Wolfpack <piafyoyo06@gmail.com>
2023-07-12 01:47:15 +0800

    feat: 新增元件內容

--- components/ComeFromAI.js
+++ components/ComeFromAI.js
@@ -11,6 +11,29 @@ const AIToolWrapper = styled.main`
   }
 `;
 
+const ComeFromAIToolTitle = styled.h2`
+  color: #000;
+  font-size: 80px;
+  margin: 0 0 80px;
+`;
+
 export default function ComeFromAI() {
-  return <AIToolWrapper className="container-fluid"></AIToolWrapper>;
+  return (
+    <AIToolWrapper className="container-fluid">
+      <ComeFromAIToolTitle className="fw-bolder text-center">
+        這些超酷的應用，都來自 AI工具王
+      </ComeFromAIToolTitle>
+      <div className="input-group mb-3">
+        <span className="input-group-text" id="basic-addon1">
+          @
+        </span>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="輸入關鍵字搜尋"
+          aria-label="search"
+        />
+      </div>
+    </AIToolWrapper>
+  );
 }
